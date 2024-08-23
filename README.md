3641d6defd72b191074843367d842742db593585
JosaaXC <josafat719@gmail.com>
2024-08-22 22:38:03 -0600

    Add Docker Compose configuration for MySQL database

---
+++ README.md
@@ -0,0 +1,16 @@
+
+# Instalación. 
+
+Para instalar las dependencias en Spring Boot, puedes utilizar el siguiente comando:
+
+```bash
+mvn install
+```
+
+Este comando ejecutará Maven y descargará todas las dependencias necesarias para tu proyecto de Spring Boot.
+
+# Levantar base de datos: 
+
+docker compose up -d
+
+# Inciar aplicación spring boot.
---
+++ src/main/java/com/java/products/models/Product.java
@@ -0,0 +1,55 @@
+package com.java.products.models;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity(name = "products")
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id; 
+
+    @Column(nullable = false, length = 50)
+    private String name;
+
+    @Column()
+    private double price;
+
+    public Product() {
+    }
+
+    public Product(int id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+}
